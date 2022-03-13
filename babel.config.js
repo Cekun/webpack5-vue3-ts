987c376239ed330d455a46a26b8b3a80561aef6a
cekun <cekung@gmail.com>
2022-03-13 16:49:06 +0800

    vant 引入；babel JS

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+const presets = [
+  ["@babel/preset-env", {
+      "useBuiltIns": 'usage', // 这里配置usage 会自动根据你使用的方法以及你配置的浏览器支持版本引入对于的方法。
+      "corejs": "3.21.1" // 指定 corejs 版本 
+  }]
+]
+const plugins = [
+  ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+  }, 'vant']
+]
+module.exports = {
+  plugins,
+  presets
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   plugins: {
     autoprefixer: {
       Browserslist: ["Android >= 4.0", "iOS >= 7", "chrome > 31"],
     },
+
     "postcss-pxtorem": {
-      rootValue({ file }) {
-        return file.indexOf("vant") !== -1 ? 37.5 : 75;
-      },
-      // rootValue: 37.5, //结果为：设计稿元素尺寸/37.5，比如元素宽375px,最终页面会换算成 10rem
-      propList: ["*"],
+      rootValue: 37.5, // 数字|函数）表示根元素字体大小或根据input参数返回根元素字体大小
+      propList: ["*"], // 使用通配符*启用所有属性
+      mediaQuery: true, // 允许在媒体查询中转换px
+      selectorBlackList: [".norem"], // 过滤掉.norem-开头的class，不进行rem转换
     },
   },
 };
