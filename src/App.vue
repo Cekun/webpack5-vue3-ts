7f4e1ec03664269033809b51947edb93077ea724
cekun <cekung@gmail.com>
2022-03-13 10:36:33 +0800

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="title">{{title}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent ({
+  setup () {
+    const title = ref('渐进式JavaScript框架');
+  
+    return {
+      title
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  background-color: antiquewhite;
+}
+</style>
---
+++ src/views/app-switch/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p class="title">{{title}}</p>
+    <img src="~/img/bg1.jpg" >
+  </div>
+  <div>
+    呵呵呵a Vue kiss 撒旦法f
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent ({
+  setup () {
+    const title = ref('app switch');
+  
+    return {
+      title
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  background-color: antiquewhite;
+  font-size: 24px;
+}
+</style>
